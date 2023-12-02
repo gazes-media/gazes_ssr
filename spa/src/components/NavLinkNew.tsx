0fce091e0cbcccda735dbf86acc586b9dde3607f
garder500 <jeremy27.clara22@gmail.com>
2023-12-03 00:47:49 +0100

    Adding a badge when historic got updated

--- spa/src/components/NavLinkNew.tsx
+++ spa/src/components/NavLinkNew.tsx
@@ -1,7 +1,8 @@
 import { NavLink } from "@mantine/core";
 import { useLocation } from "react-router-dom";
 
-export default function NavLinkNew({label, navigate, location, leftSection}: { label: string, navigate: Function, location: string, leftSection: React.ReactNode }) {
+export default function NavLinkNew({label, navigate, location, leftSection, rightSection}: { label: string, navigate: Function, location: string, leftSection: React.ReactNode, rightSection?: React.ReactNode}) {
    let currentLocation = useLocation().pathname;
+   if(rightSection) return <NavLink label={label} onClick={() => navigate(location)} active={location == currentLocation} leftSection={leftSection} rightSection={rightSection} />
     return <NavLink label={label} onClick={() => navigate(location)} active={location == currentLocation} leftSection={leftSection} />
 }
--- spa/src/components/Shell.tsx
+++ spa/src/components/Shell.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Burger, Flex, Text, Input, Image, rem, Kbd, NavLink as NavButton, Avatar } from "@mantine/core";
+import { AppShell, Burger, Flex, Text, Input, Image, rem, Kbd, NavLink as NavButton, Avatar, Badge } from "@mantine/core";
 import NavLink from "../components/NavLinkNew";
 import { useDisclosure, } from '@mantine/hooks';
 import { IconCalendar, IconHistory, IconHome, IconSearch, IconUser } from "@tabler/icons-react";
@@ -9,13 +9,16 @@ import { Spotlight, SpotlightActionData, spotlight } from '@mantine/spotlight';
 import { StoreContext } from "../Context/MainContext";
 import { auth } from "../utils/database";
 import { GoogleAuthProvider, signInWithPopup, signOut } from '@firebase/auth';
+import { Anime, AnimeWatched, latest } from "../utils/apiFetcher";
+import { getAnimeList } from "../utils/storage";
+import { convertEpisodeToNumber } from "../utils/util";
 
 export default function Shell({ child, currentRoute}: { child: React.ReactNode, currentRoute:string }) {
   const [opened, { toggle }] = useDisclosure();
   const navigate = useNavigate();
   const [animeSearched, setAnimes] = useState<SpotlightActionData[]>([]);
+  let [historyUpdated, setHistoryUpdated] = useState<AnimeWatched[]>([]);
   const { animes } = useContext(StoreContext);
-
   function signIn() {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
@@ -35,7 +38,20 @@ export default function Shell({ child, currentRoute}: { child: React.ReactNode, 
             onClick: () => { navigate("/anime/"+anime.id) },
             leftSection: <Image src={anime.url_image} width={20} height={20} radius="xl" />,
         }
-    })
+    });
+
+      (async () => {
+        let last = await latest();
+        let history = await getAnimeList();
+        let filtered = history.filter((anime) => {
+          let currentAnime = animes.find(e => e.id == anime.id) as Anime;
+          let recent = last.find(e => e.anime_url == currentAnime.url);
+          return recent && convertEpisodeToNumber(recent.episode) > anime.episode;
+        })
+        console.log(filtered);
+        setHistoryUpdated(filtered);
+
+      })();
       setAnimes(animesWithImage);
   }, [])
       
@@ -84,7 +100,7 @@ export default function Shell({ child, currentRoute}: { child: React.ReactNode, 
         <NavLink label="Accueil" leftSection={<IconHome />} navigate={navigate} location="/"  />
         <NavLink label="Chercher" leftSection={<IconSearch />} navigate={navigate} location="/search" />
         <NavLink label="Dernières sorties" leftSection={<IconCalendar />} navigate={navigate} location="/latest" />
-        <NavLink label="Historique" leftSection={<IconHistory />} navigate={navigate} location="/history" />
+        <NavLink label="Historique" leftSection={<IconHistory />} navigate={navigate} location="/history" rightSection={historyUpdated.length > 0 && <DisplayBadgeAnimeToWatch animewatched={historyUpdated} />} />
         {!auth.currentUser ? <NavButton label={"Connexion"} onClick={signIn} leftSection={<IconUser/>} /> : <NavButton label={"Déconnexion"} onClick={signOutUser} leftSection={<Avatar src={auth.currentUser.photoURL}/>} />}
       </AppShell.Navbar>
       <AppShell.Main>
@@ -92,3 +108,13 @@ export default function Shell({ child, currentRoute}: { child: React.ReactNode, 
       </AppShell.Main>
     </AppShell>  
 }
+
+function DisplayBadgeAnimeToWatch({animewatched}: {animewatched: AnimeWatched[]}) {
+    if(animewatched.length > 0){
+      return (
+        <Badge style={{ position: "relative", top: 0, right: 0 }} color="red">+{animewatched.length}</Badge>
+      )
+    }else{
+      return (<></>)
+    }
+  }
