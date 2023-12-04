dc4398e1e6ceacbf7d6dfeebf5ffabebccdb359a
garder500 <jeremy27.clara22@gmail.com>
2023-12-04 23:09:05 +0100

    Adding Cache Limit (50 is max)

--- functions/cacheManager.go
+++ functions/cacheManager.go
@@ -21,6 +21,9 @@ func (c *Cache) Set(key string, value interface{}) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 	c.data[key] = value
+	if c.GetLength() > 50 {
+		c.RemoveLast()
+	}
 	// auto remove after 12 hours
 	go func() {
 		time.Sleep(12 * time.Hour)
@@ -42,4 +45,25 @@ func (c *Cache) Remove(key string) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 	delete(c.data, key)
+}
+
+func (c *Cache) RemoveLast() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	for k := range c.data {
+		data, _ := c.Get(k)
+		if data.(string) != "downloading" {
+			e := os.Remove("videos/" + k + ".mp4") // remove the video from the disk
+			if e == nil {
+				delete(c.data, k)
+			}
+		}
+		return
+	}
+}
+
+func (c *Cache) GetLength() int {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	return len(c.data)
 }
